39591f71b6fa9a67f74ef37be1bfed40507a597b
xzc <xiazhengchun_job@12.com>
2024-03-05 00:25:23 +0800

    完成官方第一节教程

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 夏正春 on 2024/3/4.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
